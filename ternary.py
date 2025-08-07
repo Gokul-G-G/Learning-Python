4186fd1c1c0f69f09487a546b439658642c09ca6
Gokul Gopi <gokulraja143@gmail.com>
2025-08-07 12:23:38 +0530

    New file created for ternary operations

---
+++ ternary.py
@@ -0,0 +1,15 @@
+# Single line or Terneray Operator
+ 
+"""<var> = <val1> if <condition> else <val2>
+    food = input("food : ")
+    eat = "yes" if food == "cake" else "no" 
+    print(eat)"""
+
+#  clever if
+
+# <var> = (false_val,true_val) [<condition>]
+"""
+age = int(input("age : "))
+vote = ("yes","no") [age >=18]
+"""
+ 
