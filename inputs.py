d27b03093bd1f122c2b8ccea2425e865f62e4231
Gokul Gopi <gokulraja143@gmail.com>
2025-08-07 11:26:59 +0530

    New file created for study inputs in python

---
+++ inputs.py
@@ -0,0 +1,8 @@
+# Taking input from user and printing it
+
+name = input("name: ") #String input
+age = int(input("age: ")) #Integer input
+price = float(input("price :")) #Floating value input
+print(name)
+print(age)
+print(price)
