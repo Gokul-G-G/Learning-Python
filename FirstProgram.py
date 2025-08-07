21a64084579365d6cd9d49697233e5a7d636c8eb
Gokul Gopi <gokulraja143@gmail.com>
2025-08-07 11:24:55 +0530

    Some comments added about Identifiers

--- FirstProgram.py
+++ FirstProgram.py
@@ -3,3 +3,13 @@ name ="Gokul" #String
 age = 28 #integer
 salary=15000
 print(name)
+
+
+# Rules for identifiers
+"""1.Identifiers can be combination of uppercase and lowercase letters ,digits or an underscore(_).
+   so myVariable,variable_1,variable_for_print will are valid python identifiers.
+ 2.An identifier can not start with digit.So while variable1 is valid,1variable is not valid.
+ 3.We can't use special symbols like !,#,@,%,$ etc in our identifier.
+ 4.Identifier can be of any length (Simple,Short & Meaningful)"""
+
+# PYTHON IS CASE SENSITIVE LANGUAGE
