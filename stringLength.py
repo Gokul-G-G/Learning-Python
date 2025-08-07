af55b47e1ff5e9f59398cc80343dfc75cc23d111
Gokul Gopi <gokulraja143@gmail.com>
2025-08-07 21:11:30 +0530

    create a new file study string functions

---
+++ stringLength.py
@@ -0,0 +1,7 @@
+# Write a program to input user's first name & print its length.
+name = input("Enter your first name : ")
+print(len(name))
+
+# write a program to find the occurence of "$" in a string.
+word = input("Enter string : ")
+print(word.count("$"))
