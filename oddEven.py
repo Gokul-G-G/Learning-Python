d23330d59e335948a54460b03c8427b5bb39281e
Gokul Gopi <gokulraja143@gmail.com>
2025-08-08 19:14:08 +0530

    odd or even created

---
+++ oddEven.py
@@ -0,0 +1,7 @@
+# Write a program to check if a number entered by the user is odd or even
+
+num =  int(input("Enter a number to check : "))
+if( num % 2 == 0 ):
+    print("Even number")
+else:
+    print("Odd number")
