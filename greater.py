34a9b50c501884475443ce38cb598c814e9cf8db
Gokul Gopi <gokulraja143@gmail.com>
2025-08-09 15:46:05 +0530

    greater of three numbers

--- greater.py
+++ greater.py
@@ -2,10 +2,10 @@
 num1 = int(input("Enter 3 numbers : "))
 num2 = int(input())
 num3 = int(input())
-if(num1 > num2):
-    if(num1 > num3):
-        print(num1,"is bigger")
-if(num3 > num2):
-    print(num3,"is bigger")
+if(num1 > num2 and num1 > num3):
+    print("Number first is bigger",num1)
+elif(num3 > num2):
+    print("Number third is bigger",num3)
 else:
-    print(num2,"is bigger")
+    print("Number second is bigger",num2)
+ 
